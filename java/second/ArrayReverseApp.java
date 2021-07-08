341bd0c05272ea339e2bce834bdde92e64b7ebe3
Jochizan <remnyachizot2015@gmail.com>
2021-07-08 08:42:10 -0500

    Adding mejorado ReverseArrayApp

--- java/second/ArrayReverseApp.java
+++ java/second/ArrayReverseApp.java
@@ -3,11 +3,16 @@ package second;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class ArrayReverse {
+public class ArrayReverseApp {
 
     public static void main(String[] args) {
         Integer[] intArr = {4, 8, 15, 16, 23, 42};
         String[] strArr = {"Jorge", "Paco", "Pedro", "Juan", "Marta"};
+
+
+        System.out.println(Arrays.asList(intArr));
+        System.out.println(Arrays.asList(strArr));
+        System.out.println("Invirtiendo...");
 
         Collections.reverse(Arrays.asList(intArr));
         Collections.reverse(Arrays.asList(strArr));
